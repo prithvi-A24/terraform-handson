e867ffd6b77c080b93a85ae7cb4c0cfa50ae026a
prithvi-A24 <squareshift.co>
2026-08-27 05:20:45 +0000

    changed region  us-central1 from us-west1

--- task1/variables.tf
+++ task1/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "Deployment region"
   type        = string
-  default     = "us-west1"
+  default     = "us-central1"
 }
 
 variable "zone" {
